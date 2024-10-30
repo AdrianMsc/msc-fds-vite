e861ffb7609bd339b89580aca857f83e8ad73b6e
AdrianLona <adrian.lona@mscsdirect.com>
2024-10-30 11:36:19 -0600

    feat: React Router Init

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <ul>
+        {/* <li>
+          <Link to="/home">Home</Link>
+        </li> */}
+        <li>
+          <Link to="/getting-started">Getting Started</Link>
+        </li>
+        <li>
+          <Link to="/component-status">Component Status</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Home;
