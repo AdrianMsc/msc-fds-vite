704c6b4c40c5ef43ac4632172564f336f6d54f0d
diegomsc <diego.delacruz@mscdirect.com>
2025-01-21 09:22:00 -0600

    Feat: Sidebar Context

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,15 +1,25 @@
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import MscLogo from "../assets/MscLogo";
 import { Link } from "react-router-dom";
+import SidebarContext from "../context/SidebarCtx";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const context = useContext(SidebarContext);
+
+  if (!context) {
+    throw new Error("Navbar must be used within a SidebarProvider");
+  }
+
+  const { toggleSidebar } = context;
+
   return (
     <header className="bg-white overflow-hidden shadow p-5 flex w-screen fixed justify-between">
       <Link to="/">
         <MscLogo />
       </Link>
-      <button className="sm:hidden">
+      <button className="sm:hidden" onClick={toggleSidebar}>
         <FontAwesomeIcon icon={faBars} />
       </button>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SidebarProvider } from "./context/SidebarCtx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
+  <SidebarProvider>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </StrictMode>
-  </QueryClientProvider>
+    </QueryClientProvider>
+  </SidebarProvider>
 );
