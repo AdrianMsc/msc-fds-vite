0317f906d0f0d415ad54763ffb8fda18a1b12f8b
AdrianLona <adrian.lona@mscsdirect.com>
2025-02-18 15:44:09 -0600

    msc status component bar

---
+++ src/components/MscStatusComponentBar/MscStatusComponentBar.tsx
@@ -0,0 +1,22 @@
+const MscStatusComponentBar = () => {
+  return (
+    <>
+      <ul className="bg-white w-fit p-3 rounded flex space-x-3 mb-4">
+        <li>
+          <b>Guidelines:</b> 🧱
+        </li>
+        <li>
+          <b>Figma:</b> 🧱
+        </li>
+        <li>
+          <b>Storybook:</b> 🧱
+        </li>
+        <li>
+          <b>CDN:</b> 🧱
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default MscStatusComponentBar;
