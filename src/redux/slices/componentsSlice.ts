8cd18f76c93aed79bb5c49b229d420559ce8fc91
Digudev <1.5.dg.diego@gmail.com>
2025-02-25 08:44:44 -0600

    Persist new config

--- src/redux/slices/componentsSlice.ts
+++ src/redux/slices/componentsSlice.ts
@@ -1,32 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { ICategoryApi } from "../../interfaces/component.interface";
+import { createSlice } from '@reduxjs/toolkit';
+import { ICategoryApi } from '../../interfaces/component.interface';
 
 const initialState: ICategoryApi[] = [];
 
 export const componentSlice = createSlice({
-  name: "componentsReducer",
-  initialState,
-  reducers: {
-    setComponentsState: (_, action) => {
-      return action.payload;
-    },
-    deleteComponentAction: (state, action) => {
-      console.log(action.payload);
-      return state.map((item) => {
-        if (item.category === action.payload.category) {
-          return {
-            ...item,
-            components: item.components.filter(
-              (comp) => comp.name !== action.payload.name
-            ),
-          };
-        }
-        return item;
-      });
-    },
-  },
+	name: 'componentsReducer',
+	initialState,
+	reducers: {
+		setComponentsState: (_, action) => {
+			return action.payload;
+		},
+		deleteComponentAction: (state, action) => {
+			console.log(action.payload);
+			return state.map((item) => {
+				if (item.category === action.payload.category) {
+					return {
+						...item,
+						components: item.components.filter((comp) => comp.name !== action.payload.name)
+					};
+				}
+				return item;
+			});
+		}
+	}
 });
 
-export const { setComponentsState, deleteComponentAction } =
-  componentSlice.actions;
+export const { setComponentsState, deleteComponentAction } = componentSlice.actions;
 export default componentSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,30 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import rootReducer from "./rootReducer";
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './rootReducer';
+
+// Migration function to modify persisted state
+const migrations = {
+	0: (state: any) => {
+		// Modify the persisted state here (e.g., rename or delete properties)
+		if (state && state.someOldKey) {
+			state.newKey = state.someOldKey;
+			delete state.someOldKey;
+		}
+		return state;
+	}
+};
 
 const persistConfig = {
-  key: "root",
-  storage,
+	key: 'root',
+	storage,
+	version: 1, // Increase this when modifying persisted state
+	migrate: createMigrate(migrations, { debug: true }) // Migrate old state when needed
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST"],
-      },
-    }),
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+			}
+		})
 });
 
 const persistor = persistStore(store);
 
 export { persistor, store };
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
