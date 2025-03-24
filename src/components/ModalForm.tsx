ec4b13e6b044adcbccf360f137ce214db0b7335e
Digudev <1.5.dg.diego@gmail.com>
2025-03-24 10:22:34 -0600

    Set update component fixed

--- src/components/ModalForm.tsx
+++ src/components/ModalForm.tsx
@@ -1,262 +1,235 @@
-import React, { ChangeEvent, FormEvent, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "../redux/store";
-import {
-  updateField,
-  resetForm,
-  IFormState,
-  setComponentData,
-} from "../redux/slices/formSlice";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
-import {
-  IComponentApi,
-  IComponentForm,
-} from "../interfaces/component.interface";
-import { addComponent, editComponent } from "../redux/slices/componentsSlice";
+import React, { ChangeEvent, FormEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '../redux/store';
+import { updateField, resetForm, IFormState, setComponentData } from '../redux/slices/formSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { IComponentApi } from '../interfaces/component.interface';
+import { addComponent, editComponent } from '../redux/slices/componentsSlice';
 
 interface ModalFormProps {
-  triggerModal: string;
-  toggleModal: () => void;
-  title: string;
-  buttonOne: string;
-  buttonTwo: string;
-  selectedRecord: IComponentApi;
+	triggerModal: string;
+	toggleModal: () => void;
+	setSelectedRecord: (component: IComponentApi) => void;
+	title: string;
+	buttonOne: string;
+	buttonTwo: string;
+	selectedRecord: IComponentApi;
+	emptyValues: IComponentApi;
 }
 
 const ModalForm: React.FC<ModalFormProps> = ({
-  triggerModal,
-  toggleModal,
-  title,
-  buttonOne,
-  buttonTwo = "Cancel",
-  // selectedRecord,
+	triggerModal,
+	toggleModal,
+	title,
+	buttonOne,
+	buttonTwo = 'Cancel',
+	selectedRecord,
+	setSelectedRecord,
+	emptyValues
 }) => {
-  const dispatch = useDispatch<AppDispatch>();
-  const formState = useSelector((state: RootState) => state.form);
+	const dispatch = useDispatch<AppDispatch>();
+	const formState = useSelector((state: RootState) => state.form);
 
-  // useEffect(() => {
-  //   if (selectedRecord.name === "") {
-  //     dispatch(resetForm());
-  //   } else {
-  //     console.log(selectedRecord);
-  //     const formattedData: any = {
-  //       id: selectedRecord.id,
-  //       name: selectedRecord.name,
-  //       category: selectedRecord.category,
-  //       comment: selectedRecord.comment,
-  //       cdn: selectedRecord.statuses[0].cdn,
-  //       figma: selectedRecord.statuses[0].figma,
-  //       guidelines: selectedRecord.statuses[0].guidelines,
-  //       storybook: selectedRecord.statuses[0].storybook,
-  //     };
-  //     console.log(formattedData);
-  //     dispatch(setComponentData(formattedData));
-  //   }
-  // }, [selectedRecord]);
+	useEffect(() => {
+		console.log(selectedRecord);
+		if (selectedRecord.name === '') {
+			dispatch(resetForm(0));
+		} else {
+			console.log(selectedRecord);
+			const formattedData: any = {
+				id: selectedRecord.id,
+				name: selectedRecord.name,
+				category: selectedRecord.category,
+				comment: selectedRecord.comment,
+				cdn: selectedRecord.statuses[0].cdn,
+				figma: selectedRecord.statuses[0].figma,
+				guidelines: selectedRecord.statuses[0].guidelines,
+				storybook: selectedRecord.statuses[0].storybook
+			};
+			console.log(formattedData);
+			dispatch(setComponentData(formattedData));
+		}
+	}, [selectedRecord]);
 
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
-  ) => {
-    dispatch(
-      updateField({
-        field: e.target.name as keyof IFormState,
-        value: e.target.value,
-      })
-    );
-  };
+	const handleCancel = () => {
+		dispatch(resetForm(0));
+		setSelectedRecord(emptyValues);
+		toggleModal();
+	};
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    if (formState.id == "") {
-      const componentCasted = {
-        ...formState,
-        id: Number(formState.id),
-      };
-      dispatch(addComponent(componentCasted));
-    } else {
-      const componentFormatted = {
-        id: Number(formState.id),
-        name: formState.name,
-        category: formState.category,
-        comment: formState.comment,
-        statuses: [
-          {
-            guidelines: formState.guidelines,
-            figma: formState.figma,
-            storybook: formState.storybook,
-            cdn: formState.cdn,
-          },
-        ],
-      };
-      console.log("Formatted Component", componentFormatted);
-      dispatch(editComponent(componentFormatted));
-    }
-    toggleModal();
-    dispatch(resetForm());
-  };
+	const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+		dispatch(
+			updateField({
+				field: e.target.name as keyof IFormState,
+				value: e.target.value
+			})
+		);
+	};
 
-  return (
-    <div className={`msc-modal-bg ${triggerModal}`}>
-      <div className="msc-modal">
-        <div className="msc-modal-header">
-          <h4 className="msc-modal-title">{title}</h4>
-          <button id="modalClsBtn" onClick={toggleModal}>
-            <FontAwesomeIcon icon={faClose} />
-          </button>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="msc-modal-body pb-4 !w-fit">
-            <div className="flex flex-col gap-5">
-              <div className="flex gap-2 w-[600px]">
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="name" className="font-bold">
-                    Name
-                  </label>
-                  <input
-                    name="id"
-                    type="text"
-                    className="hidden"
-                    value={formState.id}
-                    onChange={handleChange}
-                  />
-                  <input
-                    name="name"
-                    type="text"
-                    className="msc-input !w-full"
-                    value={formState.name}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="category" className="font-bold">
-                    Category
-                  </label>
-                  <select
-                    name="category"
-                    className="msc-input !p-2"
-                    value={formState.category}
-                    onChange={handleChange}
-                  >
-                    <option value="">-- Select a category --</option>
-                    <option value="Foundations">Foundations</option>
-                    <option value="Action">Action</option>
-                    <option value="Form">Form</option>
-                    <option value="Indicator">Indicator</option>
-                    <option value="Layout">Layout</option>
-                    <option value="Navigation">Navigation</option>
-                    <option value="Overlay">Overlay</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex gap-2 w-[600px]">
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="guidelines" className="font-bold">
-                    Guidelines
-                  </label>
-                  <select
-                    name="guidelines"
-                    className="msc-input !p-2"
-                    value={formState.guidelines}
-                    onChange={handleChange}
-                  >
-                    <option value="🧱">🧱 Todo</option>
-                    <option value="🔨">🔨 WIP</option>
-                    <option value="🔭">🔭 Alpha</option>
-                    <option value="🧪">🧪 Beta</option>
-                    <option value="✅">✅ Live</option>
-                    <option value="🚫">🚫 Not Applicable</option>
-                  </select>
-                </div>
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="figma" className="font-bold">
-                    Figma
-                  </label>
-                  <select
-                    name="figma"
-                    className="msc-input !p-2"
-                    value={formState.figma}
-                    onChange={handleChange}
-                  >
-                    <option value="🧱">🧱 Todo</option>
-                    <option value="🔨">🔨 WIP</option>
-                    <option value="🔭">🔭 Alpha</option>
-                    <option value="🧪">🧪 Beta</option>
-                    <option value="✅">✅ Live</option>
-                    <option value="🚫">🚫 Not Applicable</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex gap-2 w-[600px]">
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="storybook" className="font-bold">
-                    Storybook
-                  </label>
-                  <select
-                    name="storybook"
-                    className="msc-input !p-2"
-                    value={formState.storybook}
-                    onChange={handleChange}
-                  >
-                    <option value="🧱">🧱 Todo</option>
-                    <option value="🔨">🔨 WIP</option>
-                    <option value="🔭">🔭 Alpha</option>
-                    <option value="🧪">🧪 Beta</option>
-                    <option value="✅">✅ Live</option>
-                    <option value="🚫">🚫 Not Applicable</option>
-                  </select>
-                </div>
-                <div className="flex flex-col gap-1 !w-[50%]">
-                  <label htmlFor="cdn" className="font-bold">
-                    CDN
-                  </label>
-                  <select
-                    name="cdn"
-                    className="msc-input !p-2"
-                    value={formState.cdn}
-                    onChange={handleChange}
-                  >
-                    <option value="🧱">🧱 Todo</option>
-                    <option value="🔨">🔨 WIP</option>
-                    <option value="🔭">🔭 Alpha</option>
-                    <option value="🧪">🧪 Beta</option>
-                    <option value="✅">✅ Live</option>
-                    <option value="🚫">🚫 Not Applicable</option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex flex-col gap-1">
-                <label htmlFor="comment" className="font-bold">
-                  Comments
-                </label>
-                <textarea
-                  name="comment"
-                  className="msc-input !p-2"
-                  value={formState.comment}
-                  onChange={handleChange}
-                ></textarea>
-              </div>
-            </div>
-          </div>
-          <div className="msc-modal-footer">
-            <button type="submit" className="msc-btn msc-btn-blue-solid w-full">
-              {buttonOne}
-            </button>
-            <button
-              type="reset"
-              onClick={() => {
-                dispatch(resetForm());
-                toggleModal();
-              }}
-              className="msc-btn msc-btn-blue-outline w-full"
-            >
-              {buttonTwo}
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		if (formState.id == '') {
+			const componentCasted = {
+				...formState,
+				id: Number(formState.id)
+			};
+			dispatch(addComponent(componentCasted));
+		} else {
+			const componentFormatted = {
+				id: Number(formState.id),
+				name: formState.name,
+				category: formState.category,
+				comment: formState.comment,
+				statuses: [
+					{
+						guidelines: formState.guidelines,
+						figma: formState.figma,
+						storybook: formState.storybook,
+						cdn: formState.cdn
+					}
+				]
+			};
+			console.log('Formatted Component', componentFormatted);
+			dispatch(editComponent(componentFormatted));
+		}
+		toggleModal();
+		dispatch(resetForm(0));
+	};
+
+	return (
+		<div className={`msc-modal-bg ${triggerModal}`}>
+			<div className="msc-modal">
+				<div className="msc-modal-header">
+					<h4 className="msc-modal-title">{title}</h4>
+					<button id="modalClsBtn" onClick={toggleModal}>
+						<FontAwesomeIcon icon={faClose} />
+					</button>
+				</div>
+				<form onSubmit={handleSubmit}>
+					<div className="msc-modal-body pb-4 !w-fit">
+						<div className="flex flex-col gap-5">
+							<div className="flex gap-2 w-[600px]">
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="name" className="font-bold">
+										Name
+									</label>
+									<input name="id" type="text" className="hidden" value={formState.id} onChange={handleChange} />
+									<input
+										name="name"
+										type="text"
+										className="msc-input !w-full"
+										value={formState.name}
+										onChange={handleChange}
+									/>
+								</div>
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="category" className="font-bold">
+										Category
+									</label>
+									<select name="category" className="msc-input !p-2" value={formState.category} onChange={handleChange}>
+										<option value="">-- Select a category --</option>
+										<option value="Foundations">Foundations</option>
+										<option value="Action">Action</option>
+										<option value="Form">Form</option>
+										<option value="Indicator">Indicator</option>
+										<option value="Layout">Layout</option>
+										<option value="Navigation">Navigation</option>
+										<option value="Overlay">Overlay</option>
+									</select>
+								</div>
+							</div>
+							<div className="flex gap-2 w-[600px]">
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="guidelines" className="font-bold">
+										Guidelines
+									</label>
+									<select
+										name="guidelines"
+										className="msc-input !p-2"
+										value={formState.guidelines}
+										onChange={handleChange}
+									>
+										<option value="🧱">🧱 Todo</option>
+										<option value="🔨">🔨 WIP</option>
+										<option value="🔭">🔭 Alpha</option>
+										<option value="🧪">🧪 Beta</option>
+										<option value="✅">✅ Live</option>
+										<option value="🚫">🚫 Not Applicable</option>
+									</select>
+								</div>
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="figma" className="font-bold">
+										Figma
+									</label>
+									<select name="figma" className="msc-input !p-2" value={formState.figma} onChange={handleChange}>
+										<option value="🧱">🧱 Todo</option>
+										<option value="🔨">🔨 WIP</option>
+										<option value="🔭">🔭 Alpha</option>
+										<option value="🧪">🧪 Beta</option>
+										<option value="✅">✅ Live</option>
+										<option value="🚫">🚫 Not Applicable</option>
+									</select>
+								</div>
+							</div>
+							<div className="flex gap-2 w-[600px]">
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="storybook" className="font-bold">
+										Storybook
+									</label>
+									<select
+										name="storybook"
+										className="msc-input !p-2"
+										value={formState.storybook}
+										onChange={handleChange}
+									>
+										<option value="🧱">🧱 Todo</option>
+										<option value="🔨">🔨 WIP</option>
+										<option value="🔭">🔭 Alpha</option>
+										<option value="🧪">🧪 Beta</option>
+										<option value="✅">✅ Live</option>
+										<option value="🚫">🚫 Not Applicable</option>
+									</select>
+								</div>
+								<div className="flex flex-col gap-1 !w-[50%]">
+									<label htmlFor="cdn" className="font-bold">
+										CDN
+									</label>
+									<select name="cdn" className="msc-input !p-2" value={formState.cdn} onChange={handleChange}>
+										<option value="🧱">🧱 Todo</option>
+										<option value="🔨">🔨 WIP</option>
+										<option value="🔭">🔭 Alpha</option>
+										<option value="🧪">🧪 Beta</option>
+										<option value="✅">✅ Live</option>
+										<option value="🚫">🚫 Not Applicable</option>
+									</select>
+								</div>
+							</div>
+							<div className="flex flex-col gap-1">
+								<label htmlFor="comment" className="font-bold">
+									Comments
+								</label>
+								<textarea
+									name="comment"
+									className="msc-input !p-2"
+									value={formState.comment}
+									onChange={handleChange}
+								></textarea>
+							</div>
+						</div>
+					</div>
+					<div className="msc-modal-footer">
+						<button type="submit" className="msc-btn msc-btn-blue-solid w-full">
+							{buttonOne}
+						</button>
+						<button type="reset" onClick={handleCancel} className="msc-btn msc-btn-blue-outline w-full">
+							{buttonTwo}
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default ModalForm;
--- src/redux/slices/formSlice.ts
+++ src/redux/slices/formSlice.ts
@@ -1,45 +1,42 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IComponentForm } from "../../interfaces/component.interface";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IComponentForm } from '../../interfaces/component.interface';
 
 export interface IFormState {
-  id: string;
-  name: string;
-  category: string;
-  guidelines: string;
-  figma: string;
-  storybook: string;
-  cdn: string;
-  comment: string;
+	id: string;
+	name: string;
+	category: string;
+	guidelines: string;
+	figma: string;
+	storybook: string;
+	cdn: string;
+	comment: string;
 }
 
 const initialState: IFormState = {
-  id: "",
-  name: "",
-  category: "",
-  guidelines: "🧱",
-  figma: "🧱",
-  storybook: "🧱",
-  cdn: "🧱",
-  comment: "",
+	id: '',
+	name: '',
+	category: '',
+	guidelines: '🧱',
+	figma: '🧱',
+	storybook: '🧱',
+	cdn: '🧱',
+	comment: ''
 };
 
 const formSlice = createSlice({
-  name: "form",
-  initialState,
-  reducers: {
-    updateField: (
-      state,
-      action: PayloadAction<{ field: keyof IFormState; value: string }>
-    ) => {
-      const { field, value } = action.payload;
-      state[field] = value;
-    },
-    setComponentData: (_, action: PayloadAction<IComponentForm>) => {
-      console.log(action.payload);
-      return action.payload;
-    },
-    resetForm: (_) => initialState,
-  },
+	name: 'form',
+	initialState,
+	reducers: {
+		updateField: (state, action: PayloadAction<{ field: keyof IFormState; value: string }>) => {
+			const { field, value } = action.payload;
+			state[field] = value;
+		},
+		setComponentData: (_, action: PayloadAction<IComponentForm>) => {
+			console.log(action.payload);
+			return action.payload;
+		},
+		resetForm: (_) => initialState
+	}
 });
 
 export const { updateField, resetForm, setComponentData } = formSlice.actions;
