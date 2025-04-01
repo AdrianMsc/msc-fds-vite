11c0e54199416c3c432049df044de19333003c3d
Digudev <1.5.dg.diego@gmail.com>
2025-04-01 09:11:04 -0600

    STYLE: Icons added

--- src/pages/Dropdown/MscDropdownPage.tsx
+++ src/pages/Dropdown/MscDropdownPage.tsx
@@ -1,96 +1,84 @@
-import { useState } from "react";
-import ComponentLayout from "../../layout/ComponentLayout/ComponentLayout";
-import Codeblock from "../../layout/Codeblock";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+import ComponentLayout from '../../layout/ComponentLayout/ComponentLayout';
+import Codeblock from '../../layout/Codeblock';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 const MscDropdownPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isOptionSelected, setIsOptionSelected] = useState(false);
-  const [selectedValue, setSelectedValue] = useState(null);
-  const [selectedDoubleValue, setSelectedDoubleValue] = useState({
-    p1: "Est. Wed, Apr 03 from PA",
-    p2: "Standard UPS Ground",
-  });
-  const [selectedNumericValue, setSelectedNumericValue] = useState();
-  const options = ["Buyer 1", "Buyer 2", "Buyer 3"];
-  const numericOptions = ["25", "50", "75", "100"];
-  const doubleOptions = [
-    {
-      p1: "Est. Thu, Apr 04 from PA",
-      p2: "UPS Ground Freight Saver",
-    },
-    {
-      p1: "Est. Wed, Apr 03 from PA",
-      p2: "1 Day UPS Air (Red)",
-    },
-    {
-      p1: "Est. Thu, Apr 03 from PA",
-      p2: "1 Day UPS (Early AM)",
-    },
-  ];
-  const placeholder = "Placeholder";
+	const [isOpen, setIsOpen] = useState(false);
+	const [isOptionSelected, setIsOptionSelected] = useState(false);
+	const [selectedValue, setSelectedValue] = useState(null);
+	const [selectedDoubleValue, setSelectedDoubleValue] = useState({
+		p1: 'Est. Wed, Apr 03 from PA',
+		p2: 'Standard UPS Ground'
+	});
+	const [selectedNumericValue, setSelectedNumericValue] = useState();
+	const options = ['Buyer 1', 'Buyer 2', 'Buyer 3'];
+	const numericOptions = ['25', '50', '75', '100'];
+	const doubleOptions = [
+		{
+			p1: 'Est. Thu, Apr 04 from PA',
+			p2: 'UPS Ground Freight Saver'
+		},
+		{
+			p1: 'Est. Wed, Apr 03 from PA',
+			p2: '1 Day UPS Air (Red)'
+		},
+		{
+			p1: 'Est. Thu, Apr 03 from PA',
+			p2: '1 Day UPS (Early AM)'
+		}
+	];
+	const placeholder = 'Placeholder';
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
 
-  const handleNumericOptionClick = (value: any) => {
-    setSelectedNumericValue(value);
-    setIsOpen(false);
-  };
+	const handleNumericOptionClick = (value: any) => {
+		setSelectedNumericValue(value);
+		setIsOpen(false);
+	};
 
-  const handleNumericInput = (e: any) => {
-    e.preventDefault();
-    setIsOpen(false);
-    setSelectedNumericValue(e.target.value);
-  };
+	const handleNumericInput = (e: any) => {
+		e.preventDefault();
+		setIsOpen(false);
+		setSelectedNumericValue(e.target.value);
+	};
 
-  const handleOptionClick = (option: any) => {
-    setSelectedValue(option);
-    setIsOpen(!isOpen);
-    setIsOptionSelected(true);
-  };
-  const handleDoubleOptionClick = (option: any) => {
-    setSelectedDoubleValue(option);
-    setIsOpen(!isOpen);
-    setIsOptionSelected(true);
-  };
+	const handleOptionClick = (option: any) => {
+		setSelectedValue(option);
+		setIsOpen(!isOpen);
+		setIsOptionSelected(true);
+	};
+	const handleDoubleOptionClick = (option: any) => {
+		setSelectedDoubleValue(option);
+		setIsOpen(!isOpen);
+		setIsOptionSelected(true);
+	};
 
-  return (
-    <ComponentLayout>
-      <div>
-        <h3 className="mb-2">Default</h3>
-        <div
-          className={`dropdown-container ${
-            isOpen ? "dropdown-active" : ""
-          } !w-[400px]`}
-          onClick={toggleDropdown}
-        >
-          <button className="dropdown-button">
-            <p className="dropdown-placeholder">
-              {selectedValue ?? placeholder}{" "}
-              <span className="text-red-600">*</span>
-            </p>
-            <i className="fa-solid fa-caret-down"></i>
-          </button>
-          <div
-            className={`dropdown-options-container ${isOpen ? "" : "hidden"}`}
-          >
-            {options.map((option, index) => (
-              <div
-                key={index}
-                className="dropdown-option"
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="my-5">
-          <Codeblock>
-            {`
+	return (
+		<ComponentLayout>
+			<div>
+				<h3 className="mb-2">Default</h3>
+				<div className={`dropdown-container ${isOpen ? 'dropdown-active' : ''} !w-[400px]`} onClick={toggleDropdown}>
+					<button className="dropdown-button">
+						<p className="dropdown-placeholder">
+							{selectedValue ?? placeholder} <span className="text-red-600">*</span>
+						</p>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</button>
+					<div className={`dropdown-options-container ${isOpen ? '' : 'hidden'}`}>
+						{options.map((option, index) => (
+							<div key={index} className="dropdown-option" onClick={() => handleOptionClick(option)}>
+								{option}
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="my-5">
+					<Codeblock>
+						{`
     <div id="dropdown" class="dropdown-container">
       <button class="dropdown-button">
         <p class="dropdown-placeholder">
@@ -115,44 +103,32 @@ const MscDropdownPage = () => {
     });
     </script>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">With Label</h3>
-        <div className="dropdown-label">
-          <p>Label</p>
-          <div
-            className={`dropdown-container ${
-              isOpen ? "dropdown-active" : ""
-            } !w-[400px]`}
-            onClick={toggleDropdown}
-          >
-            <button className="dropdown-button">
-              <p className="dropdown-placeholder">
-                {selectedValue ?? placeholder}{" "}
-                <span className="text-red-600">*</span>
-              </p>
-              <i className="fa-solid fa-caret-down"></i>
-            </button>
-            <div
-              className={`dropdown-options-container ${isOpen ? "" : "hidden"}`}
-            >
-              {options.map((option, index) => (
-                <div
-                  key={index}
-                  className="dropdown-option"
-                  onClick={() => handleOptionClick(option)}
-                >
-                  {option}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">With Label</h3>
+				<div className="dropdown-label">
+					<p>Label</p>
+					<div className={`dropdown-container ${isOpen ? 'dropdown-active' : ''} !w-[400px]`} onClick={toggleDropdown}>
+						<button className="dropdown-button">
+							<p className="dropdown-placeholder">
+								{selectedValue ?? placeholder} <span className="text-red-600">*</span>
+							</p>
+							<FontAwesomeIcon icon={faCaretDown} />
+						</button>
+						<div className={`dropdown-options-container ${isOpen ? '' : 'hidden'}`}>
+							{options.map((option, index) => (
+								<div key={index} className="dropdown-option" onClick={() => handleOptionClick(option)}>
+									{option}
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div class="dropdown-label">
       <p>Label</p>
       <div id="dropdownTwo" class="dropdown-container">
@@ -180,45 +156,28 @@ const MscDropdownPage = () => {
     });
     </script>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">With Label on top</h3>
-        <div
-          className={`dropdown-container ${
-            isOpen ? "dropdown-active" : ""
-          } !w-[400px]`}
-          onClick={toggleDropdown}
-        >
-          <p
-            className={`dropdown-label-top ${isOptionSelected ? "" : "hidden"}`}
-          >
-            {placeholder}
-          </p>
-          <button className="dropdown-button">
-            <p className={`dropdown-placeholder `}>
-              {selectedValue ?? placeholder}
-            </p>
-            <i className="fa-solid fa-caret-down"></i>
-          </button>
-          <div
-            className={`dropdown-options-container ${isOpen ? "" : "hidden"}`}
-          >
-            {options.map((option, index) => (
-              <div
-                key={index}
-                className="dropdown-option"
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">With Label on top</h3>
+				<div className={`dropdown-container ${isOpen ? 'dropdown-active' : ''} !w-[400px]`} onClick={toggleDropdown}>
+					<p className={`dropdown-label-top ${isOptionSelected ? '' : 'hidden'}`}>{placeholder}</p>
+					<button className="dropdown-button">
+						<p className={`dropdown-placeholder `}>{selectedValue ?? placeholder}</p>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</button>
+					<div className={`dropdown-options-container ${isOpen ? '' : 'hidden'}`}>
+						{options.map((option, index) => (
+							<div key={index} className="dropdown-option" onClick={() => handleOptionClick(option)}>
+								{option}
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div id="dropdownThree" class="dropdown-container">
       <p id="labelTop" class="dropdown-label-top hidden">
         Select a Buyer
@@ -251,63 +210,57 @@ const MscDropdownPage = () => {
     });
     </script>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">Double Line</h3>
-        <div
-          className={`msc-double-dropdown-container ${
-            isOpen ? "double-dropdown-active" : ""
-          } !w-[400px]`}
-          onClick={toggleDropdown}
-        >
-          <button className="dropdown-button">
-            <div className="text-left">
-              {selectedDoubleValue ? (
-                <>
-                  <p className="text-sm">
-                    {selectedDoubleValue.p1.slice(0, 5)}
-                    <b>{selectedDoubleValue.p1.slice(5, 16)}</b>
-                    {selectedDoubleValue.p1.slice(16, 24)}
-                  </p>
-                  <p className="text-xs">{selectedDoubleValue.p2}</p>
-                </>
-              ) : (
-                <>
-                  <p className="text-sm">
-                    Est. <b>Wed, Apr 03</b> from PA
-                  </p>
-                  <p className="text-xs">Standard UPS Ground</p>
-                </>
-              )}
-            </div>
-            <i className="fa-solid fa-caret-down"></i>
-          </button>
-          <div
-            className={`double-dropdown-options-container ${
-              isOpen ? "" : "hidden"
-            }`}
-          >
-            {doubleOptions.map((option, index) => (
-              <div
-                key={index}
-                className="double-dropdown-option text-left"
-                onClick={() => handleDoubleOptionClick(option)}
-              >
-                <p className="text-sm">
-                  {option.p1.slice(0, 5)}
-                  <b>{option.p1.slice(5, 16)}</b>
-                  {option.p1.slice(16, 24)}
-                </p>
-                <p className="text-xs">{option.p2}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">Double Line</h3>
+				<div
+					className={`msc-double-dropdown-container ${isOpen ? 'double-dropdown-active' : ''} !w-[400px]`}
+					onClick={toggleDropdown}
+				>
+					<button className="dropdown-button">
+						<div className="text-left">
+							{selectedDoubleValue ? (
+								<>
+									<p className="text-sm">
+										{selectedDoubleValue.p1.slice(0, 5)}
+										<b>{selectedDoubleValue.p1.slice(5, 16)}</b>
+										{selectedDoubleValue.p1.slice(16, 24)}
+									</p>
+									<p className="text-xs">{selectedDoubleValue.p2}</p>
+								</>
+							) : (
+								<>
+									<p className="text-sm">
+										Est. <b>Wed, Apr 03</b> from PA
+									</p>
+									<p className="text-xs">Standard UPS Ground</p>
+								</>
+							)}
+						</div>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</button>
+					<div className={`double-dropdown-options-container ${isOpen ? '' : 'hidden'}`}>
+						{doubleOptions.map((option, index) => (
+							<div
+								key={index}
+								className="double-dropdown-option text-left"
+								onClick={() => handleDoubleOptionClick(option)}
+							>
+								<p className="text-sm">
+									{option.p1.slice(0, 5)}
+									<b>{option.p1.slice(5, 16)}</b>
+									{option.p1.slice(16, 24)}
+								</p>
+								<p className="text-xs">{option.p2}</p>
+							</div>
+						))}
+					</div>
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div id="double-dropdown" class="msc-double-dropdown-container">
       <button class="dropdown-button">
         <div class="text-left">
@@ -337,44 +290,35 @@ const MscDropdownPage = () => {
       });
     </script>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">Numeric</h3>
-        <div
-          className={`numeric-dropdown-container ${
-            isOpen ? "dropdown-active" : ""
-          }`}
-          onClick={toggleDropdown}
-        >
-          <div className="numeric-dropdown">
-            <input
-              type="number"
-              placeholder="25"
-              className="numeric-dropdown-input"
-              onChange={handleNumericInput}
-              value={selectedNumericValue || ""}
-            />
-            <FontAwesomeIcon icon={faCaretDown} />
-          </div>
-          {isOpen && (
-            <div className="numeric-dropdown-options-container !relative !right-[75px] top-1">
-              {numericOptions.map((option) => (
-                <div
-                  key={option}
-                  className="numeric-dropdown-options"
-                  onClick={() => handleNumericOptionClick(option)}
-                >
-                  {option}
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">Numeric</h3>
+				<div className={`numeric-dropdown-container ${isOpen ? 'dropdown-active' : ''}`} onClick={toggleDropdown}>
+					<div className="numeric-dropdown">
+						<input
+							type="number"
+							placeholder="25"
+							className="numeric-dropdown-input"
+							onChange={handleNumericInput}
+							value={selectedNumericValue || ''}
+						/>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</div>
+					{isOpen && (
+						<div className="numeric-dropdown-options-container !relative !right-[75px] top-1">
+							{numericOptions.map((option) => (
+								<div key={option} className="numeric-dropdown-options" onClick={() => handleNumericOptionClick(option)}>
+									{option}
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div id="numDropdown" class="numeric-dropdown-container">
       <div class="numeric-dropdown">
         <input type="text" placeholder="25" class="numeric-dropdown-input" />
@@ -398,25 +342,23 @@ const MscDropdownPage = () => {
     });
     </script>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">Error</h3>
-        <div>
-          <div className="dropdown-container dropdown-error !w-[400px]">
-            <button className="dropdown-button">
-              <p className="dropdown-placeholder dropdown-error">
-                {placeholder} *
-              </p>
-              <i className="fa-solid fa-caret-down"></i>
-            </button>
-          </div>
-          <p className="dropdown-error-message">Error Message</p>
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">Error</h3>
+				<div>
+					<div className="dropdown-container dropdown-error !w-[400px]">
+						<button className="dropdown-button">
+							<p className="dropdown-placeholder dropdown-error">{placeholder} *</p>
+							<FontAwesomeIcon icon={faCaretDown} />
+						</button>
+					</div>
+					<p className="dropdown-error-message">Error Message</p>
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div>
       <div id="dropdownDisabled" class="dropdown-container dropdown-error">
         <button class="dropdown-button">
@@ -427,25 +369,25 @@ const MscDropdownPage = () => {
       <p class="dropdown-error-message">Error Message</p>
     </div>
           `}
-          </Codeblock>
-        </div>
-      </div>
-      <div>
-        <h3 className="mb-2">Disabled</h3>
-        <div className="dropdown-label dropdown-disabled">
-          <p>Label</p>
-          <div className="dropdown-container dropdown-disabled !w-[400px]">
-            <button className="dropdown-button dropdown-disabled" disabled>
-              <p className="dropdown-placeholder dropdown-disabled">
-                {placeholder} <span className="text-red-600">*</span>
-              </p>
-              <i className="fa-solid fa-caret-down"></i>
-            </button>
-          </div>
-        </div>
-        <div className="mt-5">
-          <Codeblock>
-            {`
+					</Codeblock>
+				</div>
+			</div>
+			<div>
+				<h3 className="mb-2">Disabled</h3>
+				<div className="dropdown-label dropdown-disabled">
+					<p>Label</p>
+					<div className="dropdown-container dropdown-disabled !w-[400px]">
+						<button className="dropdown-button dropdown-disabled" disabled>
+							<p className="dropdown-placeholder dropdown-disabled">
+								{placeholder} <span className="text-red-600">*</span>
+							</p>
+							<FontAwesomeIcon icon={faCaretDown} />
+						</button>
+					</div>
+				</div>
+				<div className="mt-5">
+					<Codeblock>
+						{`
     <div class="dropdown-label">
       <p>Label</p>
       <div id="dropdownDisabled" class="dropdown-container">
@@ -458,11 +400,11 @@ const MscDropdownPage = () => {
       </div>
     </div>
           `}
-          </Codeblock>
-        </div>
-      </div>
-    </ComponentLayout>
-  );
+					</Codeblock>
+				</div>
+			</div>
+		</ComponentLayout>
+	);
 };
 
 export default MscDropdownPage;
--- src/pages/Filter/MscFilterPage.tsx
+++ src/pages/Filter/MscFilterPage.tsx
@@ -1,31 +1,27 @@
-import Codeblock from "../../layout/Codeblock";
-import ComponentLayout from "../../layout/ComponentLayout/ComponentLayout";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Codeblock from '../../layout/Codeblock';
+import ComponentLayout from '../../layout/ComponentLayout/ComponentLayout';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const MscFilterPage = () => {
-  const filters = [
-    "Drill Bit Material",
-    "Drill Point Angle",
-    "Coating/Finish",
-    "Point Type",
-    "Cutting Direction",
-  ];
+	const filters = ['Drill Bit Material', 'Drill Point Angle', 'Coating/Finish', 'Point Type', 'Cutting Direction'];
 
-  return (
-    <ComponentLayout>
-      <div className="msc-top-filters-section">
-        <h4 className="msc-top-filters-title">Refine your search</h4>
-        <div className="msc-top-filters-container">
-          {filters.map((filter, index) => (
-            <button key={index} className="msc-top-filter group">
-              <p>{filter}</p>
-              <i className="fa-solid fa-plus group-hover:text-black"></i>
-            </button>
-          ))}
-        </div>
-      </div>
-      <div className="py-5">
-        <Codeblock>
-          {`
+	return (
+		<ComponentLayout>
+			<div className="msc-top-filters-section">
+				<h4 className="msc-top-filters-title">Refine your search</h4>
+				<div className="msc-top-filters-container">
+					{filters.map((filter, index) => (
+						<button key={index} className="msc-top-filter group">
+							<p>{filter}</p>
+							<FontAwesomeIcon icon={faPlus} />
+						</button>
+					))}
+				</div>
+			</div>
+			<div className="py-5">
+				<Codeblock>
+					{`
   <div class="msc-top-filters-section">
     <h4 class="msc-top-filters-title">Refine your search</h4>
     <div class="msc-top-filters-container">
@@ -52,39 +48,39 @@ const MscFilterPage = () => {
     </div>
   </div>
           `}
-        </Codeblock>
-      </div>
+				</Codeblock>
+			</div>
 
-      <h3 className="mb-2">Active</h3>
-      <div className="msc-top-filters-section">
-        <h4 className="msc-top-filters-title">Refine your search</h4>
-        <div className="msc-top-filters-container">
-          <button className="msc-top-filter">
-            <p>Drill Bit Material</p>
-            <span className="msc-top-filter-badge">3</span>
-          </button>
-          <button className="msc-top-filter">
-            <p>Drill Point Angle</p>
-            <span className="msc-top-filter-badge">3</span>
-          </button>
-          <button className="msc-top-filter">
-            <p>Coating/Finish</p>
-            <span className="msc-top-filter-badge">3</span>
-          </button>
-          <button className="msc-top-filter">
-            <p>Point Type</p>
-            <span className="msc-top-filter-badge">3</span>
-          </button>
-          <button className="msc-top-filter">
-            <p>Cutting Direction</p>
-            <span className="msc-top-filter-badge">3</span>
-          </button>
-        </div>
-      </div>
+			<h3 className="mb-2">Active</h3>
+			<div className="msc-top-filters-section">
+				<h4 className="msc-top-filters-title">Refine your search</h4>
+				<div className="msc-top-filters-container">
+					<button className="msc-top-filter">
+						<p>Drill Bit Material</p>
+						<span className="msc-top-filter-badge">3</span>
+					</button>
+					<button className="msc-top-filter">
+						<p>Drill Point Angle</p>
+						<span className="msc-top-filter-badge">3</span>
+					</button>
+					<button className="msc-top-filter">
+						<p>Coating/Finish</p>
+						<span className="msc-top-filter-badge">3</span>
+					</button>
+					<button className="msc-top-filter">
+						<p>Point Type</p>
+						<span className="msc-top-filter-badge">3</span>
+					</button>
+					<button className="msc-top-filter">
+						<p>Cutting Direction</p>
+						<span className="msc-top-filter-badge">3</span>
+					</button>
+				</div>
+			</div>
 
-      <div className="py-5">
-        <Codeblock>
-          {`
+			<div className="py-5">
+				<Codeblock>
+					{`
   <div class="msc-top-filters-section">
     <h4 class="msc-top-filters-title">Refine your search</h4>
     <div class="msc-top-filters-container">
@@ -111,10 +107,10 @@ const MscFilterPage = () => {
     </div>
   </div>
           `}
-        </Codeblock>
-      </div>
-    </ComponentLayout>
-  );
+				</Codeblock>
+			</div>
+		</ComponentLayout>
+	);
 };
 
 export default MscFilterPage;
