89d566455f425cfbe28dd2f25fc6e7c23cabd554
AdrianLona <adrian.lona@mscsdirect.com>
2025-04-09 11:00:33 -0600

    docs layout fix

--- src/layout/DocsLayout.tsx
+++ src/layout/DocsLayout.tsx
@@ -17,14 +17,12 @@ const DocsLayout = () => {
     <>
       <main className="flex flex-row  bg-off_white h-screen w-screen overflow-hidden">
         <Sidebar />
-        <div className="flex flex-col w-full place-content-between">
-          <div className="flex flex-col overflow-y-scroll overflow-x-visible w-full h-full  place-content-between relative">
-            <Navbar />
-            <div className="px-8 md:px-15 xl:px-20 w-full pb-4 mt-4">
-              <Outlet context={{ toggleModal }} />
-            </div>
-            <Footer toggleModal={toggleModal} />
+        <div className="flex flex-col overflow-y-scroll overflow-x-visible w-full">
+          <Navbar />
+          <div className="px-8 md:px-15 xl:px-20 pb-4 mt-4 flex-1">
+            <Outlet context={{ toggleModal }} />
           </div>
+          <Footer toggleModal={toggleModal} />
         </div>
       </main>
       <Toast />
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,20 +1,27 @@
 interface FooterProps {
-	toggleModal: () => void;
+  toggleModal: () => void;
 }
 
 const Footer = ({ toggleModal }: FooterProps) => {
-	return (
-		<div className="flex flex-col md:flex-row items-center w-full py-4 px-6 md:px-15 xl:px-20 bg-white place-content-between md:items-end h-fit">
-			<button className="msc-btn msc-btn-sm msc-btn-blue-outline md:order-2" onClick={toggleModal}>
-				Give Feedback
-			</button>
-			<div className="flex flex-row text-center content-center mt-2">
-				<p className="text-xs text-gray-500 text-nowrap self-center">All Rights Reserved</p>
-				<div className="border-l mx-3 border-gray-400"></div>
-				<p className="text-xs text-gray-500 self-center">© 2000 - 2025 MSC Industrial Direct Co., Inc.</p>
-			</div>
-		</div>
-	);
+  return (
+    <footer className="flex flex-col md:flex-row items-center w-full py-4 px-6 md:px-15 xl:px-20 bg-white justify-between md:items-end h-fit">
+      <button
+        className="msc-btn msc-btn-sm msc-btn-blue-outline md:order-2"
+        onClick={toggleModal}
+      >
+        Give Feedback
+      </button>
+      <div className="flex flex-row text-center content-center mt-2">
+        <p className="text-xs text-gray-500 text-nowrap self-center">
+          All Rights Reserved
+        </p>
+        <div className="border-l mx-3 border-gray-400"></div>
+        <p className="text-xs text-gray-500 self-center">
+          © 2000 - 2025 MSC Industrial Direct Co., Inc.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
