7e26a9432dd68388df9863149a6b897cb7bfd620
diegomsc <diego.delacruz@mscdirect.com>
2025-04-09 12:57:35 -0600

    Update home styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,6 +18,22 @@ export default function Home() {
   const [hsFlag, setHsFlag] = useState("");
   const [isFading, setIsFading] = useState(false);
   const data = useSelector((state: RootState) => state.components);
+  const [lottieSize, setLottieSize] = useState(600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.outerWidth < 500) {
+        setLottieSize(400);
+      } else {
+        setLottieSize(600);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial call to set the size based on the current window size
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const defaultOptions = {
     loop: true,
@@ -41,7 +57,7 @@ export default function Home() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   return (
-    <main className="flex flex-col justify-center items-center h-screen m-auto md:flex-row">
+    <main className="flex flex-col sm:justify-center items-center h-screen m-auto md:flex-row">
       <span
         className={`fixed top-3 right-3 transition-opacity duration-1000 ${
           isFading ? "opacity-0" : "opacity-100"
@@ -50,74 +66,82 @@ export default function Home() {
         {hsFlag ? hsFlag : <MscMiniLoading />}
       </span>
 
-      <div className="flex flex-col justify-center px-8 md:pl-16 w-full md:w-[45%] items-center h-screen">
-        <h1 className="font-normal text-[3rem] xl:text-[4rem] leading-none mb-3">
-          {isAuthenticated && (
-            <small className="text-lg text-primary-blue font-bold">
-              {isLoading ? "Loading" : `Hello ${user?.name}`}
-            </small>
-          )}
-          <br />
-          Welcome to MSC Design System
-        </h1>
-        <h3 className="font-light text-pretty self-start">
-          Empowering Innovation Through Unified Design.
-        </h3>
+      <div className="flex flex-col md:flex-row my-auto">
+        <div className="flex flex-col md:justify-center px-2 md:pl-16 w-full md:w-[45%] items-center h-fit md:h-screen">
+          <div className="w-full text-center md:text-start">
+            <h1 className="font-normal text-[3rem] xl:text-[4rem] leading-none mb-3 text-pretty">
+              {isAuthenticated && (
+                <small className="text-lg text-primary-blue font-bold">
+                  {isLoading ? "Loading" : `Hello ${user?.name}`}
+                </small>
+              )}
+              <br />
+              Welcome to MSC Design System
+            </h1>
+            <h3 className="font-light text-pretty self-start">
+              Empowering Innovation Through Unified Design.
+            </h3>
+          </div>
 
-        <div className="py-5 flex flex-wrap place-content-between md:w-full items-center justify-center md:justify-normal">
-          <Link
-            className="msc-btn msc-btn-blue-solid mr-5"
-            to="/docs/Gettingstarted"
-          >
-            Go to Docs
-            <FontAwesomeIcon
-              icon={faArrowRight}
-              className="ml-2"
-              height={16}
-              width={16}
-            />
-          </Link>
-
-          {isAuthenticated ? (
+          <div className="py-5 gap-3 flex flex-wrap place-content-around md:place-content-between md:w-full sm:items-center sm:justify-center md:justify-normal">
             <Link
-              className="msc-text-link py-3 mr-4"
-              to="https://adrianmsc.github.io/msc-fuel-design-system-react/?path=/docs/configure-your-project--docs"
+              className="msc-btn msc-btn-blue-solid order-2"
+              to="/docs/Gettingstarted"
             >
-              Storybook
+              Go to Docs
               <FontAwesomeIcon
-                icon={faBook}
-                className="mr-5 ml-2 text-[#ff4785]"
+                icon={faArrowRight}
+                className="ml-2"
                 height={16}
                 width={16}
               />
             </Link>
-          ) : (
-            ""
-          )}
 
-          <MscLoginWidget
-            className={"relative mr-4"}
-            type="button"
-            logoutBtn={false}
-          />
+            {isAuthenticated ? (
+              <Link
+                className="msc-text-link py-3"
+                to="https://adrianmsc.github.io/msc-fuel-design-system-react/?path=/docs/configure-your-project--docs"
+              >
+                Storybook
+                <FontAwesomeIcon
+                  icon={faBook}
+                  className="mr-5 ml-2 text-[#ff4785]"
+                  height={16}
+                  width={16}
+                />
+              </Link>
+            ) : (
+              ""
+            )}
 
-          <Link
-            className="msc-text-link py-3"
-            to="https://ds-blog-ten.vercel.app/"
-          >
-            Release Notes
-            <FontAwesomeIcon
-              icon={faArrowUpRightFromSquare}
-              className="ml-2"
-              height={16}
-              width={16}
+            <Link
+              className="msc-text-link py-3 order-3"
+              to="https://ds-blog-ten.vercel.app/"
+            >
+              Release Notes
+              <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                className="ml-2"
+                height={16}
+                width={16}
+              />
+            </Link>
+
+            <MscLoginWidget
+              className={"relative order-1"}
+              type="button"
+              logoutBtn={false}
             />
-          </Link>
+          </div>
         </div>
-      </div>
 
-      <div className="hidden md:block">
-        <Lottie options={defaultOptions} height={600} width={600} />
+        <div className="self-start items-center md:self-center w-full">
+          <Lottie
+            options={defaultOptions}
+            height={lottieSize}
+            width={lottieSize}
+          />
+        </div>
       </div>
     </main>
   );
