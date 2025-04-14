8d4cde3c06e596f5d9f29ebc70c95e763bbc7b6a
Digudev <1.5.dg.diego@gmail.com>
2025-04-14 13:04:27 -0600

    Feat: Inbox V0.2
    Design Updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,148 +1,111 @@
-import { useEffect, useState } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import Lottie from "react-lottie";
-import animation from "../assets/animation.json";
-import MscMiniLoading from "../components/MscMiniLoading/MscMiniLoading";
-import MscLoginWidget from "../components/MscLoginWidget/MscLoginWidget";
-import { RootState } from "../redux/store";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowRight,
-  faArrowUpRightFromSquare,
-  faBook,
-} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Lottie from 'react-lottie';
+import animation from '../assets/animation.json';
+import MscMiniLoading from '../components/MscMiniLoading/MscMiniLoading';
+import MscLoginWidget from '../components/MscLoginWidget/MscLoginWidget';
+import { RootState } from '../redux/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowUpRightFromSquare, faBook } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
-  const [hsFlag, setHsFlag] = useState("");
-  const [isFading, setIsFading] = useState(false);
-  const data = useSelector((state: RootState) => state.components);
-  const [lottieSize, setLottieSize] = useState(600);
+	const [hsFlag, setHsFlag] = useState('');
+	const [isFading, setIsFading] = useState(false);
+	const data = useSelector((state: RootState) => state.components);
+	const [lottieSize, setLottieSize] = useState(600);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.outerWidth < 500) {
-        setLottieSize(400);
-      } else {
-        setLottieSize(600);
-      }
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.outerWidth < 500) {
+				setLottieSize(400);
+			} else {
+				setLottieSize(600);
+			}
+		};
 
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial call to set the size based on the current window size
+		window.addEventListener('resize', handleResize);
+		handleResize(); // Initial call to set the size based on the current window size
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animation,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice'
+		}
+	};
 
-  useEffect(() => {
-    if (data) {
-      setHsFlag("👍");
+	useEffect(() => {
+		if (data) {
+			setHsFlag('👍');
 
-      setTimeout(() => {
-        setIsFading(true);
-      }, 1500);
-    }
-  }, [data]);
+			setTimeout(() => {
+				setIsFading(true);
+			}, 1500);
+		}
+	}, [data]);
 
-  const { user, isAuthenticated, isLoading } = useAuth0();
+	const { user, isAuthenticated, isLoading } = useAuth0();
 
-  return (
-    <main className="flex flex-col sm:justify-center items-center h-screen m-auto md:flex-row">
-      <span
-        className={`fixed top-3 right-3 transition-opacity duration-1000 ${
-          isFading ? "opacity-0" : "opacity-100"
-        }`}
-      >
-        {hsFlag ? hsFlag : <MscMiniLoading />}
-      </span>
+	return (
+		<main className="flex flex-col sm:justify-center items-center h-screen m-auto md:flex-row">
+			<span
+				className={`fixed top-3 right-3 transition-opacity duration-1000 ${isFading ? 'opacity-0' : 'opacity-100'}`}
+			>
+				{hsFlag ? hsFlag : <MscMiniLoading />}
+			</span>
 
-      <div className="flex flex-col md:flex-row my-auto">
-        <div className="flex flex-col md:justify-center px-2 md:pl-16 w-full md:w-[45%] items-center h-fit md:h-screen">
-          <div className="w-full text-center md:text-start">
-            <h1 className="font-normal text-[3rem] xl:text-[4rem] leading-none mb-3 text-pretty">
-              {isAuthenticated && (
-                <small className="text-lg text-primary-blue font-bold">
-                  {isLoading ? "Loading" : `Hello ${user?.name}`}
-                </small>
-              )}
-              <br />
-              Welcome to MSC Design System
-            </h1>
-            <h3 className="font-light text-pretty self-start">
-              Empowering Innovation Through Unified Design.
-            </h3>
-          </div>
+			<div className="flex flex-col md:flex-row my-auto">
+				<div className="flex flex-col md:justify-center px-2 md:pl-16 w-full md:w-[45%] items-center h-fit md:h-screen">
+					<div className="w-full text-center md:text-start">
+						<h1 className="font-normal text-[3rem] xl:text-[4rem] leading-none mb-3 text-pretty">
+							{isAuthenticated && (
+								<small className="text-lg text-primary-blue font-bold">
+									{isLoading ? 'Loading' : `Hello ${user?.name}`}
+								</small>
+							)}
+							<br />
+							Welcome to MSC Design System
+						</h1>
+						<h3 className="font-light text-pretty self-start">Empowering Innovation Through Unified Design.</h3>
+					</div>
 
-          <div className="py-5 gap-3 flex flex-wrap place-content-around md:place-content-between md:w-full sm:items-center sm:justify-center md:justify-normal">
-            <Link
-              className="msc-btn msc-btn-blue-solid order-2"
-              to="/docs/Gettingstarted"
-            >
-              Go to Docs
-              <FontAwesomeIcon
-                icon={faArrowRight}
-                className="ml-2"
-                height={16}
-                width={16}
-              />
-            </Link>
+					<div className="py-5 gap-3 flex flex-wrap place-content-around md:place-content-between md:w-full sm:items-center sm:justify-center md:justify-normal">
+						<Link className="msc-btn msc-btn-blue-solid order-2" to="/docs/Gettingstarted">
+							Go to Docs
+							<FontAwesomeIcon icon={faArrowRight} className="ml-2" height={16} width={16} />
+						</Link>
 
-            {isAuthenticated ? (
-              <Link
-                className="msc-text-link py-3"
-                to="https://adrianmsc.github.io/msc-fuel-design-system-react/?path=/docs/configure-your-project--docs"
-              >
-                Storybook
-                <FontAwesomeIcon
-                  icon={faBook}
-                  className="mr-5 ml-2 text-[#ff4785]"
-                  height={16}
-                  width={16}
-                />
-              </Link>
-            ) : (
-              ""
-            )}
+						{isAuthenticated ? (
+							<Link
+								className="msc-text-link py-3 order-3"
+								to="https://adrianmsc.github.io/msc-fuel-design-system-react/?path=/docs/configure-your-project--docs"
+							>
+								Storybook
+								<FontAwesomeIcon icon={faBook} className=" mr-5 ml-2 text-[#ff4785]" height={16} width={16} />
+							</Link>
+						) : (
+							''
+						)}
 
-            <Link
-              className="msc-text-link py-3 order-3"
-              to="https://ds-blog-ten.vercel.app/"
-            >
-              Release Notes
-              <FontAwesomeIcon
-                icon={faArrowUpRightFromSquare}
-                className="ml-2"
-                height={16}
-                width={16}
-              />
-            </Link>
+						<Link className="msc-text-link py-3 order-3" to="https://ds-blog-ten.vercel.app/">
+							Release Notes
+							<FontAwesomeIcon icon={faArrowUpRightFromSquare} className="ml-2" height={16} width={16} />
+						</Link>
 
-            <MscLoginWidget
-              className={"relative order-1"}
-              type="button"
-              logoutBtn={false}
-            />
-          </div>
-        </div>
+						<MscLoginWidget className={'relative order-1'} type="button" logoutBtn={false} />
+					</div>
+				</div>
 
-        <div className="self-start items-center md:self-center w-full">
-          <Lottie
-            options={defaultOptions}
-            height={lottieSize}
-            width={lottieSize}
-          />
-        </div>
-      </div>
-    </main>
-  );
+				<div className="self-start items-center md:self-center w-full">
+					<Lottie options={defaultOptions} height={lottieSize} width={lottieSize} />
+				</div>
+			</div>
+		</main>
+	);
 }
--- src/pages/Notifications/NotificationsPage.tsx
+++ src/pages/Notifications/NotificationsPage.tsx
@@ -1,118 +1,154 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { baseUrl } from "../../api";
-import { IFeedback, setFeedback } from "../../redux/slices/feedbackSlice";
-import { useDispatch } from "react-redux";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { baseUrl } from '../../api';
+import { IFeedback, setFeedback } from '../../redux/slices/feedbackSlice';
+import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 const NotificationsPage = () => {
-  const [notifications, setNotifications] = useState<IFeedback[] | null>(null);
-  const [selectedNotification, setSelectedNotification] =
-    useState<IFeedback | null>(null);
-  const [inboxHeight, setInboxHeight] = useState(695);
-  // const messages = useSelector((state: RootState) => state.feedback);
-  const dispatch = useDispatch();
+	const [notifications, setNotifications] = useState<IFeedback[] | null>(null);
+	const [selectedNotification, setSelectedNotification] = useState<IFeedback | null>(null);
+	const [inboxHeight, setInboxHeight] = useState(695);
+	const [messageWidth, setMessageWidth] = useState(0);
+	const [notiColors, setNotiColors] = useState<{ [key: string]: string }>({});
 
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      try {
-        const response = await axios.get(`${baseUrl}/inbox`);
-        setNotifications(response.data);
-        dispatch(setFeedback(response.data));
-      } catch (error) {
-        console.error("Error fetching notifications:", error);
-      }
-    };
-    fetchNotifications();
-  }, []);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    const handleResize = () => {
-      let autoHeight = window.outerHeight - 135;
-      setInboxHeight(autoHeight);
-    };
+	const colors = ['bg-blue-300', 'bg-red-300', 'bg-green-500', 'bg-yellow-400', 'bg-purple-300'];
 
-    window.addEventListener("load", handleResize);
-    window.addEventListener("resize", handleResize);
+	useEffect(() => {
+		const fetchNotifications = async () => {
+			try {
+				const response = await axios.get(`${baseUrl}/inbox`);
+				setNotifications(response.data);
+				dispatch(setFeedback(response.data));
 
-    handleResize(); // Initial call to set the size based on the current window size
+				const colorMap: { [key: string]: string } = {};
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+				response.data.forEach((noti: IFeedback) => {
+					const randomColor = colors[Math.floor(Math.random() * colors.length)];
+					colorMap[noti.email] = randomColor;
+				});
 
-  if (notifications === null) {
-    return <div>Loading...</div>;
-  }
+				setNotiColors(colorMap);
+			} catch (error) {
+				console.error('Error fetching notifications:', error);
+			}
+		};
+		fetchNotifications();
+	}, []);
 
-  return (
-    <div
-      className="flex flex-row -mx-8 md:-mx-15 xl:-mx-20 w-full self-center"
-      style={{ height: `${inboxHeight}px` }}
-    >
-      <div className="flex flex-col min-w-fit h-full bg-slate-50">
-        <div className="border-b border-gray-300 text-center py-4 min-w-[250px] max-w-[300px] ">
-          <h1 className="text-nowrap">Inbox 📥</h1>
-        </div>
-        <div className="flex flex-col w-full h-full overflow-y-scroll">
-          {notifications.map((noti) => {
-            const isSelected = selectedNotification?.id === noti.id;
-            return (
-              <button
-                key={noti.id}
-                onClick={() => setSelectedNotification(noti)}
-                className={`text-start min-w-[250px] max-w-[300px] ${
-                  isSelected ? "bg-blue-100" : "hover:bg-blue-50"
-                }`}
-              >
-                <div
-                  className="flex flex-row gap-2 pl-6 pr-3 py-3 border-b border-r border-l border-gray-300 overflow-hidden"
-                  key={noti.id}
-                >
-                  <div className="flex size-10 min-w-10 rounded-full text-center place-content-center bg-blue-300">
-                    <p className="text-xl font-semibold my-auto">
-                      {noti.name.charAt(0)}
-                    </p>
-                  </div>
-                  <div className="w-full overflow-hidden">
-                    <p className="font-bold text-blue-700">{noti.name}</p>
-                    <p className="truncate">{noti.message}</p>
-                  </div>
-                </div>
-              </button>
-            );
-          })}
-        </div>
-      </div>
-      <div className="flex w-full h-full overflow-y-scroll mt-14 p-4 ">
-        <div className="flex flex-col w-full h-full">
-          <div className="flex flex-row w-full place-content-between p-4 overflow-hidden">
-            <div className="flex flex-row gap-2 items-center p-4 overflow-hidden w-full">
-              <div className="flex size-10 min-w-10 rounded-full text-center place-content-center bg-blue-300">
-                <p className="text-xl font-semibold my-auto">
-                  {selectedNotification?.name.charAt(0)}
-                </p>
-              </div>
-              <div>
-                <h3>{selectedNotification?.name}</h3>
-                <h5 className="text-gray-500 text-xs">
-                  {selectedNotification?.email}
-                </h5>
-              </div>
-            </div>
-            <span className="pt-8 text-end text-xs text-gray-400 self-end text-nowrap">
-              {selectedNotification?.created_at?.slice(0, 10)}
-            </span>
-          </div>
-          <div className="w-full border-t border-gray-400"></div>
-          <div className="flex flex-col p-4 overflow-hidden">
-            <span className="text-blue-500  py-1 px-2 text-xs font-semibold border border-blue-500 rounded-full w-fit">
-              {selectedNotification?.status}
-            </span>
-            <p className="pt-2">{selectedNotification?.message}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const handleHeigth = () => {
+			let autoHeight = window.outerHeight - 135;
+			setInboxHeight(autoHeight);
+		};
+		const handleWidth = () => {
+			let autoWidth = window.outerWidth;
+			setMessageWidth(autoWidth);
+		};
+
+		window.addEventListener('load', handleHeigth);
+		window.addEventListener('resize', handleHeigth);
+		window.addEventListener('load', handleWidth);
+		window.addEventListener('resize', handleWidth);
+
+		handleHeigth(); // Initial call to set the size based on the current window size
+		handleWidth(); // Initial call to set the size based on the current window size
+
+		return () => {
+			window.removeEventListener('load', handleHeigth);
+			window.removeEventListener('resize', handleHeigth);
+			window.removeEventListener('load', handleWidth);
+			window.removeEventListener('resize', handleWidth);
+		};
+	}, []);
+
+	if (notifications === null) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div className=" flex flex-row -mx-8 md:-mx-15 xl:-mx-20 w-full self-center" style={{ width: `${messageWidth}px` }}>
+			<div className="flex flex-col min-w-fit h-full bg-slate-50" style={{ height: `${inboxHeight}px` }}>
+				<div className="border-b border-gray-300 text-center py-4 min-w-[250px] max-w-[300px] ">
+					<h1 className="text-nowrap">Inbox 📥</h1>
+				</div>
+				<div className="flex flex-col w-full h-full overflow-y-scroll">
+					{notifications.map((noti) => {
+						const isSelected = selectedNotification?.id === noti.id;
+						return (
+							<button
+								key={noti.id}
+								onClick={() => setSelectedNotification(noti)}
+								className={`text-start min-w-[250px] max-w-[300px] ${isSelected ? 'bg-blue-100' : 'hover:bg-blue-50'}`}
+							>
+								<div
+									className="flex flex-row gap-2 pl-6 pr-3 py-3 border-b border-r border-l border-gray-300 overflow-hidden"
+									key={noti.id}
+								>
+									<div
+										className={`flex size-10 min-w-10 rounded-full text-center place-content-center text-white ${
+											notiColors[noti.email] || 'bg-blue-300'
+										}`}
+									>
+										<p className="text-xl font-semibold my-auto">{noti.name.charAt(0)}</p>
+									</div>
+									<div className="w-full overflow-hidden">
+										<p className="font-bold">{noti.name}</p>
+										<p className="truncate text-gray-500">{noti.message}</p>
+									</div>
+								</div>
+							</button>
+						);
+					})}
+				</div>
+			</div>
+			{selectedNotification === null ? (
+				<div className="flex w-full h-full bg-transparent" style={{ height: `${inboxHeight}px` }}>
+					<div className="flex flex-col w-full h-full overflow-y-scroll">
+						<p className="text-center my-auto text-2xl text-stone-400">Select a message to view details</p>
+					</div>
+				</div>
+			) : (
+				<div className="flex h-full w-full overflow-y-scroll mt-14 p-4">
+					<div className="flex flex-col w-full h-full">
+						<div className="flex flex-row w-full place-content-between p-4 overflow-hidden">
+							<div className="flex flex-row gap-2 items-center overflow-hidden w-full">
+								<div
+									className={`flex size-10 min-w-10 rounded-full text-center place-content-center text-white ${
+										notiColors[selectedNotification?.email] || 'bg-blue-300'
+									}`}
+								>
+									<p className="text-xl font-semibold my-auto">{selectedNotification?.name.charAt(0)}</p>
+								</div>
+								<div>
+									<h3>{selectedNotification?.name}</h3>
+									<h5 className="text-gray-500 text-xs font-thin">{selectedNotification?.email}</h5>
+								</div>
+							</div>
+							<div className="flex flex-col gap-2">
+								<button className="self-end">
+									<FontAwesomeIcon icon={faEllipsisV} />
+								</button>
+								<span className="text-end text-xs text-gray-400 self-end text-nowrap">
+									{selectedNotification?.created_at?.slice(0, 10)}
+								</span>
+							</div>
+						</div>
+						<div className="w-full border-t border-gray-400"></div>
+						<div className="flex flex-col p-4 overflow-hidden">
+							<span className="w-fit rounded-full bg-blue-200 text-blue-700 text-xs font-medium me-2 px-2.5 py-0.5">
+								{selectedNotification?.status}
+							</span>
+							<p className="pt-2">{selectedNotification?.message}</p>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NotificationsPage;
