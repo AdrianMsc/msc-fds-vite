20e7822dbc3e1eb054820d7290aba0be7edc7326
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-05-05 09:22:19 -0600

    sidebar style adjustments

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -10,8 +10,6 @@ import handleDataSend from "../utils/handleDataSend"; // Importa la función
 import { getNavLinkTo } from "../utils/getNavLinkTo";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import SearchBar from "../components/SearchBar/SearchBar";
-import MscLogo from "../assets/MscLogo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
@@ -43,7 +41,7 @@ const Sidebar: React.FC = () => {
 
   return (
     <aside
-      className={`pt-5 pb-20 px-4 bg-white sm:max-w-[230px] sm:min-w-[230px] sm:flex flex-col gap-1 overflow-auto ${
+      className={`pt-5 pb-20  bg-white sm:max-w-[230px] sm:min-w-[230px] sm:flex flex-col overflow-auto ${
         isSidebarOpen
           ? "flex absolute h-lvh lg:flex lg:static lg:h-full"
           : "hidden"
@@ -59,12 +57,14 @@ const Sidebar: React.FC = () => {
         />
       </button>
 
-      <strong>Start Here</strong>
+      <strong className="px-4">Start Here</strong>
       <NavLink
         to={`/docs/${createLinkPage("GettingStarted")}`}
         onClick={toggleSidebar}
         className={({ isActive }) =>
-          isActive ? "font-bold text-primary-blue ml-5" : "ml-5"
+          isActive
+            ? "bg-monochromes-grey_xlight pl-7 py-1"
+            : "pl-7 py-1 hover:bg-monochromes-grey_xlight"
         }
       >
         Getting Started
@@ -73,19 +73,17 @@ const Sidebar: React.FC = () => {
         to={`/docs/${createLinkPage("ComponentStatus")}`}
         onClick={toggleSidebar}
         className={({ isActive }) =>
-          isActive ? "font-bold text-primary-blue ml-5" : "ml-5"
+          isActive
+            ? "bg-monochromes-grey_xlight pl-7 py-1"
+            : "pl-7 py-1 hover:bg-monochromes-grey_xlight"
         }
       >
         Component Status
       </NavLink>
-      <strong className="text-primary-blue flex items-center">
-        Categories
-        {/* {isLoading && <MscMiniLoading />} */}
-      </strong>
       {categories?.map((item, idx) => (
         <React.Fragment key={idx}>
           <strong
-            className="flex cursor-pointer"
+            className="flex cursor-pointer px-4"
             onClick={() => toggleCategory(item.category)}
           >
             {item.category}
@@ -98,7 +96,7 @@ const Sidebar: React.FC = () => {
             />
           </strong>
           <div
-            className={`flex flex-col space-y-1 ${
+            className={`flex flex-col  ${
               openCategories.includes(item.category) ? "!hidden" : ""
             }`}
           >
@@ -109,8 +107,8 @@ const Sidebar: React.FC = () => {
                   key={idx}
                   className={({ isActive }) => {
                     return isActive
-                      ? "font-bold text-primary-blue ml-5"
-                      : "ml-5";
+                      ? "bg-monochromes-grey_xlight pl-7 py-1"
+                      : "pl-7 py-1 hover:bg-monochromes-grey_xlight";
                   }}
                   to={getNavLinkTo(comp)}
                   onClick={(event) => {
