761c2fa1bee9f7dc19dbe4d01bce95061e480fc3
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-06-03 15:14:52 -0600

    Feat: Environment definitions

--- src/api/feedback/createFeedback.ts
+++ src/api/feedback/createFeedback.ts
@@ -1,19 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
-import { IFeedback } from '../../redux/slices/feedbackSlice';
+import axios, { AxiosResponse } from "axios";
+import { IFeedback } from "../../redux/slices/feedbackSlice";
+import { baseUrl } from "..";
 
-export const createFeedback = async (data: IFeedback): Promise<AxiosResponse<any>> => {
-	try {
-		const dataCasted = {
-			...data,
-			id: Number(data.id),
-			status: 'pending',
-			read: false
-		};
-		const response = await axios.post(`https://msc-component-status-ws.vercel.app/message/`, dataCasted);
-		console.log(response);
-		return response;
-	} catch (error) {
-		console.error('Error creating component:', error);
-		throw error;
-	}
+export const createFeedback = async (
+  data: IFeedback
+): Promise<AxiosResponse<any>> => {
+  try {
+    const dataCasted = {
+      ...data,
+      id: Number(data.id),
+      status: "pending",
+      read: false,
+    };
+    const response = await axios.post(baseUrl, dataCasted);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error("Error creating component:", error);
+    throw error;
+  }
 };
