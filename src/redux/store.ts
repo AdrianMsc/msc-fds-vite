482233bb5c7ee043502a008fbc81ccf7c41b702f
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-08-11 11:20:57 -0600

    feat: storage save the component data

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("reduxState");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (error) {
+    console.error("Error cargando estado desde localStorage", error);
+    return undefined;
+  }
+}
+
+function saveToLocalStorage(state: any) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("reduxState", serializedState);
+  } catch (error) {
+    console.error("Error guardando estado en localStorage", error);
+  }
+}
+
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState: loadFromLocalStorage(), // ← Se hidrata aquí
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
+});
+
+store.subscribe(() => {
+  saveToLocalStorage({
+    currentComponent: store.getState().currentComponent, // 🔹 Solo guardamos ese slice
+  });
 });
 
 export { store };
